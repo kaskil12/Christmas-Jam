54c70e8610df80322234be524d8b9b07b0f1c684
Kasper Kilde <kasperkilde2@gmail.com>
2023-12-18 23:05:09 +0100

    test

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.Animations;
+using UnityEngine.Animations;
+using UnityEngine.Animations.Rigging;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public RigBuilder rig;
+    public TwoBoneIKConstraint leftHand;
+    public TwoBoneIKConstraint rightHand;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rig.Build();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rig.Build();
     }
 }
